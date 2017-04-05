a944321cac03b5b4e6c331abc41c04b42dfdff98
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-05 16:18:45 +0200

    action box section without image ready; responsive (non float) in mobil

--- htdocs/app/sections/action-box.blade.php
+++ htdocs/app/sections/action-box.blade.php
@@ -24,7 +24,7 @@ $boxes = $block->getBox();
 								<h2 class="h2">{{$description}}</h2>
 								<br>
 								<button class="action-box__box__text__table__td__button btn btn--icon-text" type="button">
-								<i class="btn__icon__primary"></i>
+								<i class="btn__icon primary-color"></i>
 								<span class="btn__text">{{$buttontext}}</span>
 								</button>
 							</div>
