3465544345389c1d7818db636544e5fa9999e176
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-10 14:18:37 +0200

    section two basic responsive;

--- htdocs/app/sections/corp-info.blade.php
+++ htdocs/app/sections/corp-info.blade.php
@@ -1,24 +1,23 @@
 <div class="section section--corp-info">
 	<div class="section__content">
 		<div class="corp-info">
-			<div class="corp-info__inner">
-				<?php 
-				$title = $block->getTitle();
-				$subtitle = $block->getSubtitle();
-				$description = $block->getDescription();
-				$sectionimg = $block->getSectionimg();
-				?>
-				@foreach ($sectionimg as $image)
-				<div class="corp-info__info">
-					<img src="{{$image['image']}}" alt="Section Image" class="corp-info__info__image bg-image" />
-					<div class="corp-info__info__text">
-						<h1 class="corp-info__info__text__headline">{{$title}}</h1>
-						<h2 class="corp-info__info__text__subheadline">{{$subtitle}}</h2>
-						<h3 class="corp-info__info__text__description">{!!$description!!}</h3>
-					</div>
-				</div>
-				@endforeach
+			<?php 
+			$title = $block->getTitle();
+			$subtitle = $block->getSubtitle();
+			$description = $block->getDescription();
+			$sectionimg = $block->getSectionimg();
+			?>
+			@foreach ($sectionimg as $image)
+			<div class="corp-info__info">
+				<img src="{{$image['image']}}" alt="Section Image" class="corp-info__info__image" />
 			</div>
+			@endforeach
+				<div class="corp-info__info__text">
+					<h1 class="corp-info__info__text__headline">{{$title}}</h1>
+					<h2 class="corp-info__info__text__subheadline">{{$subtitle}}</h2>
+					<br>
+					<h3 class="corp-info__info__text__description">{!!$description!!}</h3>
+				</div>
 		</div>
 	</div>
 </div>
