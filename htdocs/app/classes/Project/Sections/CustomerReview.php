946d7cea3d8316404c68b9e09641de56e0435e0c
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-13 13:52:59 +0200

    section research-teaser style update

--- htdocs/app/classes/Project/Sections/CustomerReview.php
+++ htdocs/app/classes/Project/Sections/CustomerReview.php
@@ -18,7 +18,7 @@ class CustomerReview extends \A365\Wordpress\Block\AcfBlock {
 				FieldHelper::createText('description', 'Beschreibung')->setRequired(),
 				FieldHelper::createText('signature', 'Unterschrift')->setRequired(),
 				FieldHelper::createImageCrop('big_image', 'Forschungsbild groß', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'yes', 'save_format' => 'url', "width" => 620, "height" => 620])->setRequired(),
-				FieldHelper::createRepeater('links', 'Forschungsbilder', ["min" => 3, "max" => 3])
+				FieldHelper::createRepeater('customer_img', 'Forschungsbilder', ["min" => 3, "max" => 3])
 				->addSubfields([
 								FieldHelper::createImageCrop('image', 'Forschungsbild klein', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'yes', 'save_format' => 'url', "width" => 200, "height" => 200])->setRequired()
 								]),
--- htdocs/app/sections/research-teaser.blade.php
+++ htdocs/app/sections/research-teaser.blade.php
@@ -1,30 +1,35 @@
+<?php 
+	$researchimg = $block->getResearchImg();
+?>
 <div class="section section--fullwidth">
 	<div class="section__content">
 		<div class="research-teaser">
-			<div class="research-teaser__inner">
+			<div class="research-teaser__content">
 				<?php 
+				$bigimage = $block->getBigImage();
 				$title = $block->getTitle();
 				$description = $block->getDescription();
 				$buttontext = $block->getButtonText();
 				?>
-				<div class="research-teaser__content">
-					<img src="/assets/images/layout/Research_big.png" alt="Research Image big" class="research-teaser__content__image--main" />
-					<div class="research-teaser__content__image--featured">
-						<img src="/assets/images/layout/Research_small1.png" alt="Research Image small" class="research-teaser__content image--featured-img1" />
-						<img src="/assets/images/layout/Research_small2.png" alt="Research Image small" class="research-teaser__content image--featured-img2" />
-						<img src="/assets/images/layout/Research_small3.png" alt="Research Image small" class="research-teaser__content image--featured-img3" />
+				<div class="research-teaser__content__text">
+					<h2 class="h1">{{$title}}</h2>
+					<h3 class="h2">{{$description}}</h3>
+                    <button class="research-teaser__content__text__table__button btn btn--icon-text" type="button">
+                        <i class="btn__icon primary-color"></i>
+                        <span class="btn__text">{{$buttontext}}</span>
+                    </button>
+				</div>
+				<div class="research-teaser__content__images">
+					<div class="research-teaser__content__images--big">
+						<img src="{{$bigimage}}" alt="Forschungsbild groß" class="research-teaser__content__images--big__inner" />
 					</div>
-					<div class="research-teaser__content__text">
-						<div class="research-teaser__content__text__table">
-							<h2 class="h1">{{$title}}</h2>
-							<br>
-							<h2 class="h2">{{$description}}</h2>
-							<br>
-							<button class="research-teaser__content__text__table__button btn btn--icon-text" type="button">
-								<i class="btn__icon primary-color"></i>
-								<span class="btn__text">{{$buttontext}}</span>
-							</button>
-						</div>
+					<div class="research-teaser__content__images--small">
+					@foreach ($researchimg as $img)
+					<?php 
+					$smallimg = $img['image'];
+					?>
+					<img src="{{$smallimg}}" alt="Forschungsbild klein" class="research-teaser__content__images--small__inner" />
+					@endforeach
 					</div>
 				</div>
 			</div>
