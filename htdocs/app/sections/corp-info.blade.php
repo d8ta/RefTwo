716aa6b6dba6dd9dcfac04b8f226b473d3190d98
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-19 18:01:21 +0200

    refactoring first section of unternehmen subset

--- htdocs/app/sections/corp-info.blade.php
+++ htdocs/app/sections/corp-info.blade.php
@@ -1,6 +1,7 @@
 <div class="section section--corp-info">
 	<div class="section__content">
-		<div class="corp-info">
+		<div class="margin-more-top">
+			<div class="corp-info">
 			<?php 
 			$title = $block->getTitle();
 			$subtitle = $block->getSubtitle();
@@ -8,15 +9,16 @@
 			$sectionimg = $block->getSectionimg();
 			?>
 			@foreach ($sectionimg as $image)
-			<div class="corp-info__info">
-				<img src="{{$image['image']}}" alt="Section Image" class="corp-info__info__image" />
+			<div class="corp-info--img">
+				<img src="{{$image['image']}}" alt="Section Image" class="corp-image" />
 			</div>
 			@endforeach
-				<div class="corp-info__info__text">
-					<h2 class="corp-info__info__text__headline h1">{{$title}}</h2>
-					<h3 class="corp-info__info__text__subheadline">{{$subtitle}}</h3>
-					<h4 class="corp-info__info__text__description">{!!$description!!}</h4>
+				<div class="corp-info--text">
+					<h2 class="h1 primary-color">{{$title}}</h2>
+					<h2 class="h2 subtitle-color">{{$subtitle}}</h2>
+					<h3 class="h3 description-left">{!!$description!!}</h3>
 				</div>
+			</div>
 		</div>
 	</div>
 </div>
