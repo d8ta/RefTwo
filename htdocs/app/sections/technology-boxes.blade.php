2fb6977ec0d838df1e87b8f44e0904c9a9c9e8cd
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-19 09:25:11 +0200

    technologie boxes transitions ready (WIP: link to subsites needs to be added later)

--- htdocs/app/sections/technology-boxes.blade.php
+++ htdocs/app/sections/technology-boxes.blade.php
@@ -9,8 +9,8 @@
 		$title = $techbox['title'];
 		$description = $techbox['description'];
 		?>
-		<div class="technology-boxes">
-		<div href="" class="technology-boxes__inner">
+		<a href="" class="technology-boxes">
+		<div class="technology-boxes__inner">
 			<div class="technology-boxes__inner__image bg-image" style="background-image: url({{$techbox['background']}})">
 			</div>
 			<div class="technology-boxes__inner__text">
@@ -20,9 +20,14 @@
 						<div class="technology-boxes__inner__text--description__clamp">{{$description}}</div>
 					</h3>
 				</div>
+				<div class="link btn--icon-text primary-brand-btn">
+				<i class="btn__icon white-icon"></i>
+				<span class="btn__text">mehr erfahren</span>
+				</button>
+				</div>
 			</div>
 		</div>
-		</div>
+		</a>
 		@endforeach
 	</div>
 </div>
