07606a2336247c77854575122f513fae18ec2f9c
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-20 09:50:32 +0200

    basic responsiveness for images and text (WIP)

--- htdocs/app/sections/corp-info.blade.php
+++ htdocs/app/sections/corp-info.blade.php
@@ -2,21 +2,23 @@
 	<div class="section__content">
 		<div class="margin-more-top">
 			<div class="corp-info">
-			<?php 
-			$title = $block->getTitle();
-			$subtitle = $block->getSubtitle();
-			$description = $block->getDescription();
-			$sectionimg = $block->getSectionimg();
-			?>
-			@foreach ($sectionimg as $image)
-			<div class="corp-info--img">
-				<img src="{{$image['image']}}" alt="Section Image" class="corp-image" />
-			</div>
-			@endforeach
-				<div class="corp-info--text">
+				<div class="corp-info__images">	
+					<?php 
+					$title = $block->getTitle();
+					$subtitle = $block->getSubtitle();
+					$description = $block->getDescription();
+					$sectionimg = $block->getSectionimg();
+					?>
+					@foreach ($sectionimg as $image)
+						<div class="corp-info__images__img">
+							<img src="{{$image['image']}}" alt="Section Image" class="corp-image" />
+						</div>
+					@endforeach
+				</div>
+				<div class="corp-info__text margin">
 					<h2 class="h1 primary-color">{{$title}}</h2>
 					<h2 class="h2 subtitle-color">{{$subtitle}}</h2>
-					<h3 class="h3 description-left">{!!$description!!}</h3>
+					<h3 class="h3 corp-info__text--left">{!!$description!!}</h3>
 				</div>
 			</div>
 		</div>
