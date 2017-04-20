49c8ae5f0b54e269a26c1e83461b0295d75c819f
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-20 10:55:53 +0200

    core value section responsive

--- htdocs/app/sections/core-values.blade.php
+++ htdocs/app/sections/core-values.blade.php
@@ -3,21 +3,23 @@ $corevalues = $block->getCorevalues();
 ?>
 <div class="section section--core-values">
 	<div class="section__content">
-		<div class="core-values">
-			@foreach ($corevalues as $value)
-			<?php 
-			$image = $value['image'];
-			$title = $value['title'];
-			$description = $value['description'];
-			?>
-			<div class="core-values__info">
-				<img src="{{$image}}" alt="Section Image" class="core-values__info__image" />
-				<div class="core-values__info__text">
-					<h2 class="core-values__info__text__headline h1">{{$title}}</h2>
-					<h3 class="core-values__info__text__description">{!!$description!!}</h3>
+		<div class="margin">
+			<div class="core-values">
+				@foreach ($corevalues as $value)
+				<?php 
+				$image = $value['image'];
+				$title = $value['title'];
+				$description = $value['description'];
+				?>
+				<div class="core-values__info">
+					<img src="{{$image}}" alt="Section Image" class="core-values__info__image" />
+					<div class="core-values__info__text">
+						<h2 class="h2 primary-color">{{$title}}</h2>
+						<h3 class="core-values__info__text__description">{!!$description!!}</h3>
+					</div>
 				</div>
+				@endforeach
 			</div>
-			@endforeach
 		</div>
 	</div>
 </div>
