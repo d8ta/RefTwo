1f50f6326ce2e0615bdd608d3fc88fd9cba222bb
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-20 11:31:39 +0200

    environment section responsive

--- htdocs/app/sections/environment.blade.php
+++ htdocs/app/sections/environment.blade.php
@@ -1,19 +1,24 @@
 <div class="section section--fullwidth">
 	<div class="section__content">
-		<div class="environment">
+		<div class="margin">
+			<div class="environment">
 				<?php 
 				$background = $block->getBackground();
 				$title = $block->getTitle();
 				$subtitle = $block->getSubtitle();
 				$description = $block->getDescription();
 				?>
-				<div class="environment__image bg-image" style="background-image: url({{$background}})"></div>
+				<div class="environment__image bg-image" style="background-image: url({{$background}})">
+				</div>
 				<div class="environment__box">
-				<div class="environment__box__text">
-					<h2 class="h1">{{$title}}</h2>
-					<h2 class="h2">{{$subtitle}}</h2>
-					<h3 class="h3">{!!$description!!}</h3>
-				</div>	
+					<div class="environment__box__text">
+						<h2 class="h1 primary-color">{{$title}}</h2>
+						<h2 class="h2 subtitle-color">{{$subtitle}}</h2>
+							<div class="environment__box__text__description">
+								<h3 class="environment__box__text__description__clamp">{!!$description!!}</h3>
+							</div>
+					</div>	
+				</div>
 			</div>
 		</div>
 	</div>
