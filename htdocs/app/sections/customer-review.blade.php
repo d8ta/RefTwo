a242c8c1f6c5e10b78cd3cf6e468ed5bd811c5bc
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-20 11:58:41 +0200

    corporation subsite responsive and refactored

--- htdocs/app/sections/customer-review.blade.php
+++ htdocs/app/sections/customer-review.blade.php
@@ -3,30 +3,32 @@
 ?>
 <div class="section section--fullwidth">
 	<div class="section__content">
-		<div class="customer-review">
-			<div class="customer-review__content">
-				<?php 
-				$bigimage = $block->getBigImage();
-				$title = $block->getTitle();
-				$description = $block->getDescription();
-				$signature = $block->getSignature();
-				?>
-				<div class="customer-review__content__text">
-					<h2 class="h1">{{$title}}</h2>
-					<h3 class="h2">{{$description}}</h3>
-					<h3 class="h2">{{$signature}}</h3>
-				</div>
-				<div class="customer-review__content__images">
-					<div class="customer-review__content__images--big">
-						<img src="{{$bigimage}}" alt="Forschungsbild groß" class="customer-review__content__images--big__inner" />
-					</div>
-					<div class="customer-review__content__images--small">
-					@foreach ($customerimg as $img)
+		<div class="margin">
+			<div class="customer-review">
+				<div class="customer-review__content">
 					<?php 
-					$smallimg = $img['image'];
+					$bigimage = $block->getBigImage();
+					$title = $block->getTitle();
+					$description = $block->getDescription();
+					$signature = $block->getSignature();
 					?>
-					<img src="{{$smallimg}}" alt="Forschungsbild klein" class="customer-review__content__images--small__inner" />
-					@endforeach
+					<div class="customer-review__content__text">
+						<h2 class="h1">{{$title}}</h2>
+						<h3 class="h2">{{$description}}</h3>
+						<h3 class="h2">{{$signature}}</h3>
+					</div>
+					<div class="customer-review__content__images">
+						<div class="customer-review__content__images--big">
+							<img src="{{$bigimage}}" alt="Forschungsbild groß" class="customer-review__content__images--big__inner" />
+						</div>
+						<div class="customer-review__content__images--small">
+						@foreach ($customerimg as $img)
+						<?php 
+						$smallimg = $img['image'];
+						?>
+						<img src="{{$smallimg}}" alt="Forschungsbild klein" class="customer-review__content__images--small__inner" />
+						@endforeach
+						</div>
 					</div>
 				</div>
 			</div>
