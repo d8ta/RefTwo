34e22a1584bf7448ffa9c11961a102a79ca99cff
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-24 14:27:38 +0200

    scss from navigation changed (deleting comments and changing margins to em or rem)

--- htdocs/app/views/components/footer/navigation.blade.php
+++ htdocs/app/views/components/footer/navigation.blade.php
@@ -1,8 +1,8 @@
 <?php
 $items = $section->getItems();
 ?>
-<nav class="footer__inner__navigation navigation navigation--floated navigation--footer">
-	<ul class="navigation__level-1">
-		@each('components.footer.navigation.item', $items, 'item')
+<nav class="">
+	<ul class="">
+		@eaczh('components.footer.navigation.item', $items, 'item')
 	</ul>
 </nav>
