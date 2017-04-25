d4080abdc8521b4b1cd5353a86ce1ceaa25516c4
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-25 11:19:23 +0200

    fresh install of owl carousel 2

--- htdocs/app/sections/welcome-slider.blade.php
+++ htdocs/app/sections/welcome-slider.blade.php
@@ -6,32 +6,51 @@
 		<div class="section__content">
 			<div class="margin">
 				<div class="welcome-slider">
-					<div class="welcome-slider__inner js-owl-carousel">
-						@foreach ($slides as $slide)
-						<?php 
-						$title = $slide['title'];
-						$pretitle = $slide['pretitle'];
-						$description = $slide['description'];
-						$buttontext = $slide['button_text'];
-						?>
-						<div class="welcome-slider__slide">
-							<div class="welcome-slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
-							</div>
-							<div class="welcome-slider__slide__text">
-								<div class="welcome-slider__slide__text__table">
-									<div class="welcome-slider__slide__text__table__td">
-										<h1 class="h5">{{$pretitle}}</h1>
-										<h2 class="h1">{{$title}}</h2>
-										<h2 class="h2">{{$description}}</h2>
-										<button class="welcome-slider__slide__text__table__td btn--icon-text" type="button">
-											<i class="btn__icon primary-color"></i>
-											<span class="btn__text">{{$buttontext}}</span>
-										</button>
+					<div class="owl-carousel owl-theme">	
+						<div class="welcome-slider__inner">
+							@foreach ($slides as $slide)
+							<?php 
+							$title = $slide['title'];
+							$pretitle = $slide['pretitle'];
+							$description = $slide['description'];
+							$buttontext = $slide['button_text'];
+							?>
+							<div class="welcome-slider__slide">
+								<div class="welcome-slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
+								</div>
+								<div class="welcome-slider__slide__text">
+									<div class="welcome-slider__slide__text__table">
+										<div class="welcome-slider__slide__text__table__td">
+											<h1 class="h5">{{$pretitle}}</h1>
+											<h2 class="h1">{{$title}}</h2>
+											<h2 class="h2">{{$description}}</h2>
+											<button class="welcome-slider__slide__text__table__td btn--icon-text" type="button">
+												<i class="btn__icon primary-color"></i>
+												<span class="btn__text">{{$buttontext}}</span>
+											</button>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>			
-						@endforeach
+							</div>						
+							<div class="welcome-slider__slide">
+								<div class="welcome-slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
+								</div>
+								<div class="welcome-slider__slide__text">
+									<div class="welcome-slider__slide__text__table">
+										<div class="welcome-slider__slide__text__table__td">
+											<h1 class="h5">{{$pretitle}}</h1>
+											<h2 class="h1">{{$title}}</h2>
+											<h2 class="h2">{{$description}}</h2>
+											<button class="welcome-slider__slide__text__table__td btn--icon-text" type="button">
+												<i class="btn__icon primary-color"></i>
+												<span class="btn__text">{{$buttontext}}</span>
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>			
+							@endforeach
+						</div>
 					</div>
 				</div>
 			</div>
