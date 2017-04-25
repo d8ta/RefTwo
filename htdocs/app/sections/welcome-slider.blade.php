18313c00fd82a770b8a64a064cb78737e73bfea7
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-25 14:29:40 +0200

    navelements aligned with at nav element

--- htdocs/app/sections/welcome-slider.blade.php
+++ htdocs/app/sections/welcome-slider.blade.php
@@ -5,53 +5,34 @@
 	<div class="section--welcome">
 		<div class="section__content">
 			<div class="margin">
-				<div class="welcome-slider">
-					<div class="owl-carousel owl-theme">	
-						<div class="welcome-slider__inner">
-							@foreach ($slides as $slide)
-							<?php 
-							$title = $slide['title'];
-							$pretitle = $slide['pretitle'];
-							$description = $slide['description'];
-							$buttontext = $slide['button_text'];
-							?>
-							<div class="welcome-slider__slide">
-								<div class="welcome-slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
-								</div>
-								<div class="welcome-slider__slide__text">
-									<div class="welcome-slider__slide__text__table">
-										<div class="welcome-slider__slide__text__table__td">
-											<h1 class="h5">{{$pretitle}}</h1>
-											<h2 class="h1">{{$title}}</h2>
-											<h2 class="h2">{{$description}}</h2>
-											<button class="welcome-slider__slide__text__table__td btn--icon-text" type="button">
-												<i class="btn__icon primary-color"></i>
-												<span class="btn__text">{{$buttontext}}</span>
-											</button>
-										</div>
+				<div class="welcome-slider js-owl-carousel owl-carousel">
+					@foreach ($slides as $slide)
+					<?php 
+					$title = $slide['title'];
+					$pretitle = $slide['pretitle'];
+					$description = $slide['description'];
+					$buttontext = $slide['button_text'];
+					?>
+					<div class="welcome-slider__slide">
+						<div class="welcome-slider__slide__inner">
+							<div class="welcome-slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
+							</div>
+							<div class="welcome-slider__slide__text">
+								<div class="welcome-slider__slide__text__table">
+									<div class="welcome-slider__slide__text__table__td">
+										<h1 class="h5">{{$pretitle}}</h1>
+										<h2 class="h1">{{$title}}</h2>
+										<h2 class="h2">{{$description}}</h2>
+										<button class="welcome-slider__slide__text__table__td btn--icon-text" type="button">
+											<i class="btn__icon primary-color"></i>
+											<span class="btn__text">{{$buttontext}}</span>
+										</button>
 									</div>
 								</div>
-							</div>						
-							<div class="welcome-slider__slide">
-								<div class="welcome-slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
-								</div>
-								<div class="welcome-slider__slide__text">
-									<div class="welcome-slider__slide__text__table">
-										<div class="welcome-slider__slide__text__table__td">
-											<h1 class="h5">{{$pretitle}}</h1>
-											<h2 class="h1">{{$title}}</h2>
-											<h2 class="h2">{{$description}}</h2>
-											<button class="welcome-slider__slide__text__table__td btn--icon-text" type="button">
-												<i class="btn__icon primary-color"></i>
-												<span class="btn__text">{{$buttontext}}</span>
-											</button>
-										</div>
-									</div>
-								</div>
-							</div>			
-							@endforeach
+							</div>
 						</div>
-					</div>
+					</div>							
+					@endforeach
 				</div>
 			</div>
 		</div>
