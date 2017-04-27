6cefeaa8f9d1f23a2bbe15af0f8aad40c57da231
Daniel Raudschus <daniel.raudschus@a365.at>
2017-04-27 15:08:27 +0200

    Action boxes mobile ready

--- htdocs/app/sections/welcome-slider.blade.php
+++ htdocs/app/sections/welcome-slider.blade.php
@@ -15,15 +15,15 @@
 					?>
 					<div class="welcome-slider__slide">
 						<div class="welcome-slider__slide__inner">
-							<div class="welcome-slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
+							<div class="welcome-slider__slide__inner__image bg-image" style="background-image: url({{$slide['background']}})">
 							</div>
-							<div class="welcome-slider__slide__text">
-								<div class="welcome-slider__slide__text__table">
-									<div class="welcome-slider__slide__text__table__td">
+							<div class="welcome-slider__slide__inner__text">
+								<div class="welcome-slider__slide__inner__text__table">
+									<div class="welcome-slider__slide__inner__text__table__td">
 										<h1 class="h5">{{$pretitle}}</h1>
 										<h2 class="h1">{{$title}}</h2>
 										<h2 class="h2">{{$description}}</h2>
-										<button class="welcome-slider__slide__text__table__td btn--icon-text" type="button">
+										<button class="welcome-slider__slide__inner__text__table__td btn--icon-text" type="button">
 											<i class="btn__icon primary-color"></i>
 											<span class="btn__text">{{$buttontext}}</span>
 										</button>
