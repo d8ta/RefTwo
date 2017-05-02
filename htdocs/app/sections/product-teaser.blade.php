52ee6cec63710245ddf6ea597ece0ca9e7349231
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-02 17:33:12 +0200

    product teaser mobile changes

--- htdocs/app/sections/product-teaser.blade.php
+++ htdocs/app/sections/product-teaser.blade.php
@@ -10,19 +10,21 @@
 				$button_url = $block->getButtonUrl();
 				?>
 				<div class="product-teaser__inner">
-				<div class="product-teaser__inner--title">
-					<h2 class="h1 primary-color">{{$title}}</h2>
-				</div>
-				<div class="product-teaser__inner--subtitle">
-					<h2 class="h2 subtitle-color">{{$subtitle}}</h2>
-				</div>
-				<div class="product-teaser__inner--description">
-					<h3 class="h3">{{$description}}</h3>
-				</div>			
-					<button class="btn--icon-text primary-brand-btn" type="button">
-					<i class="btn__icon white-icon"></i>
-					<span class="btn__text">{{$button_text}}</span>
-					</button>
+					<div class="product-teaser__inner--title">
+						<h2 class="pagetitle">{{$title}}</h2>
+					</div>
+					<div class="product-teaser__inner--subtitle">
+						<h2 class="pagesubtitle">{{$subtitle}}</h2>
+					</div>
+					<div class="product-teaser__inner--description">
+						<p class="pagetext">{{$description}}</p>
+					</div>	
+					<div class="product-teaser__inner--button">
+						<button class="btn--icon-text primary-brand-btn" type="button">
+						<i class="btn__icon white-icon"></i>
+						<span class="btn__text">{{$button_text}}</span>
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>	
