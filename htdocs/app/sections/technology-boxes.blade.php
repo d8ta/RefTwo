6248d315fb6dc60c2e2a365ee4621655dd51f71c
Daniel Lehner <daniel.lehner@a365.at>
2017-05-03 14:48:33 +0200

    technology boxes fix + headline fix

--- htdocs/app/sections/technology-boxes.blade.php
+++ htdocs/app/sections/technology-boxes.blade.php
@@ -4,32 +4,34 @@
 ?>
 <div class="section section--technology-boxes">
 	<div class="section__content">
-		<div class="margin">
-		@foreach ($techboxes as $techbox)
-		<?php 
-		$title = $techbox['title'];
-		$description = $techbox['description'];
-		?>
-			<a href="" class="technology-boxes">
-				<div class="technology-boxes__inner">
-					<div class="technology-boxes__inner__image bg-image" style="background-image: url({{$techbox['background']}})">
-					</div>
-					<div class="technology-boxes__inner__text js-matchheight" data-mh="{{ $mh_group }}">
-						<h2 class="h4 primary-color">{{$title}}</h2>
-						<div class="technology-boxes__inner__text--description">
-							<h3 class="h3">
-								<div class="technology-boxes__inner__text--description__clamp">{{$description}}</div>
-							</h3>
+		<div class="technology-boxes">
+			@foreach ($techboxes as $techbox)
+			<?php 
+			$title = $techbox['title'];
+			$description = $techbox['description'];
+			?>
+				
+				<div class="technology-boxes__box">
+					<a href="" class="technology-boxes__box__inner">
+						<div class="technology-boxes__box__image bg-image" style="background-image: url({{$techbox['background']}})">
 						</div>
-						<div class="link btn--icon-text primary-brand-btn">
-						<i class="btn__icon white-icon"></i>
-						<span class="btn__text">mehr erfahren</span>
-						</button>
+						<div class="technology-boxes__box__text js-matchheight" data-mh="{{ $mh_group }}">
+							<h2 class="h4 primary-color">{{$title}}</h2>
+							<div class="technology-boxes__box__text--description">
+								<h3 class="h3">
+									<div class="technology-boxes__box__text--description__clamp">{{$description}}</div>
+								</h3>
+							</div>
+							<div class="link btn--icon-text primary-brand-btn">
+							<i class="btn__icon white-icon"></i>
+							<span class="btn__text">mehr erfahren</span>
+							</button>
+							</div>
 						</div>
-					</div>
+						
+					</a>
 				</div>
-			</a>
-		@endforeach
+			@endforeach
 		</div>
 	</div>
 </div>
