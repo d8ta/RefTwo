877649c7b1a0db247045eb2470dd2b141df02edd
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-03 16:34:27 +0200

    WIP: refactoring technology boxes

--- htdocs/app/sections/technology-boxes.blade.php
+++ htdocs/app/sections/technology-boxes.blade.php
@@ -10,27 +10,27 @@
 			$title = $techbox['title'];
 			$description = $techbox['description'];
 			?>
-				
-				<div class="technology-boxes__box">
-					<a href="" class="technology-boxes__box__inner">
-						<div class="technology-boxes__box__image bg-image" style="background-image: url({{$techbox['background']}})">
-						</div>
-						<div class="technology-boxes__box__text js-matchheight" data-mh="{{ $mh_group }}">
+			<div class="technology-boxes__box">
+				<a href="" class="technology-boxes__box__inner">
+					<div class="technology-boxes__box__image bg-image" style="background-image: url({{$techbox['background']}})">
+					</div>
+					<div class="technology-boxes__box__text js-matchheight" data-mh="{{ $mh_group }}">
+						<div class="technology-boxes__box__text--title">
 							<h2 class="h4 primary-color">{{$title}}</h2>
-							<div class="technology-boxes__box__text--description">
-								<h3 class="h3">
-									<div class="technology-boxes__box__text--description__clamp">{{$description}}</div>
-								</h3>
-							</div>
-							<div class="link btn--icon-text primary-brand-btn">
-							<i class="btn__icon white-icon"></i>
-							<span class="btn__text">mehr erfahren</span>
-							</button>
-							</div>
 						</div>
-						
-					</a>
-				</div>
+						<div class="technology-boxes__box__text--description">
+							<p class="h3">
+								<div class="technology-boxes__box__text--description__clamp">{{$description}}</div>
+							</p>
+						</div>
+						<div class="technology-boxes__box__text--link btn--icon-text primary-brand-btn">
+						<i class="btn__icon white-icon"></i>
+						<span class="btn__text">mehr erfahren</span>
+						</button>
+						</div>
+					</div>
+				</a>
+			</div>
 			@endforeach
 		</div>
 	</div>
