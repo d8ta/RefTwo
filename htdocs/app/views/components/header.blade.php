c3328da84ffa803eef6d9c62a4a1667ae999e9e8
Daniel Lehner <daniel.lehner@a365.at>
2017-05-08 20:16:40 +0200

    header section updated

--- htdocs/app/views/components/header.blade.php
+++ htdocs/app/views/components/header.blade.php
@@ -5,11 +5,15 @@
 ?>
 <header class="section site-header">
     <div class="section__content">
-        <div class="site-header__logo">
-            @include('components.header.logo-box')
-        </div>
-        @include('components.header.navigation')
-		@include('components.header.menu-burger')
+        <div class="site-header__inner">
+	        <div class="site-header__left">
+	            @include('components.header.logo-box')
+	        </div>
+	        <div class="site-header__right">
+	        	@include('components.header.navigation')
+	        </div>
+			@include('components.header.menu-burger')
+	    </div>
     </div>
 </header>
 
