bbc52876232f50ff4edd0aa100e3abb823870338
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-08 20:33:18 +0200

    WIP research teaser

--- htdocs/app/sections/research-teaser.blade.php
+++ htdocs/app/sections/research-teaser.blade.php
@@ -13,27 +13,40 @@ $buttontext = $block->getButtonText();
 	<div class="section section--yellow">
 		<div class="section__inner">
 			<div class="research-teaser">
-				<div class="research-teaser__content research-teaser__content--left" data-match-height>
+				<div class="research-teaser__content research-teaser__content--left" js-match-height>
 					<h2 class="h1">{{$title}}</h2>
 					<p class="h2">{{$description}}</p>
              		<button class="simple-text__link icon-link btn btn--icon-text" type="button">
                         <i class="btn__icon primary-color"></i>
                         <span class="btn__text">{{$buttontext}}</span>
                     </button>
-
-
 				</div>
-				<div class="research-teaser__content research-teaser__content--right" data-match-height>
-					{{-- <div class="research-teaser__content__image"> --}}
-						<img alt="{{$title}}" src="{{$bigimage}}" class="research-teaser__content__image">
-						<img src="assets/images/icons/entwicklung.svg" class="research-teaser__content__icon" alt="{{$title}} icon" />
-						@foreach ($researchimg as $img)
-						<?php 
-						$smallimg = $img['image'];
-						?>
-						<img src="{{$smallimg}}" alt="Forschungsbild klein" class="research-teaser__content__smallimg" />
-						@endforeach
-					{{-- </div> --}}
+				<div class="research-teaser__content research-teaser__content--right" js-match-height>
+					<img alt="{{$title}}" src="{{$bigimage}}" class="research-teaser__content__image">
+					<img src="assets/images/icons/entwicklung.svg" class="research-teaser__content__icon" alt="{{$title}} icon" />
+					@foreach ($researchimg as $img)
+					<?php 
+					$smallimg = $img['image'];
+					?>
+					<div class="research-teaser__content__smallimg">
+						<img src="{{$smallimg}}" alt="Forschungsbild klein" class="research-teaser__content__smallimg--inner" />
+					</div>
+					@endforeach
+				</div>
+				<div class="research-teaser__content__images">
+				<div class="research-teaser__content__images--big">
+					<img src="{{$bigimage}}" alt="Forschungsbild groß" class="research-teaser__content__images--big__inner" />
+					<img src="assets/images/icons/entwicklung.svg" class="research-teaser__content__images--big__icon" alt="Icon" />
+					<div class="research-teaser__whitebox"></div>
+				</div>
+				<div class="research-teaser__content__images--small">
+				@foreach ($researchimg as $img)
+				<?php 
+				$smallimg = $img['image'];
+				?>
+				<img src="{{$smallimg}}" alt="Forschungsbild klein" class="research-teaser__content__images--small__inner" />
+				@endforeach
+				</div>
 				</div>
 			</div>
 		</div>
@@ -65,8 +78,6 @@ $buttontext = $block->getButtonText();
 		                        <span class="btn__text">{{$buttontext}}</span>
 		                    </button>
 						</div>
-
-
 						<div class="research-teaser__content__images">
 							<div class="research-teaser__content__images--big">
 								<img src="{{$bigimage}}" alt="Forschungsbild groß" class="research-teaser__content__images--big__inner" />
@@ -82,8 +93,6 @@ $buttontext = $block->getButtonText();
 							@endforeach
 							</div>
 						</div>
-
-
 					</div>
 				</div>
 			</div>
