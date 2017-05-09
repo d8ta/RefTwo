0dcde07ad8da503875b48f4dfa8746c45c006ae7
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-09 18:43:03 +0200

    first changes to site

--- htdocs/app/sections/corp-info.blade.php
+++ htdocs/app/sections/corp-info.blade.php
@@ -1,25 +1,23 @@
-<div class="section section--corp-info">
+<div class="section section--corp-info section--margin">
 	<div class="section__content">
-		<div class="margin-more-top">
-			<div class="corp-info">
-				<div class="corp-info__images">	
-					<?php 
-					$title = $block->getTitle();
-					$subtitle = $block->getSubtitle();
-					$description = $block->getDescription();
-					$sectionimg = $block->getSectionimg();
-					?>
-					@foreach ($sectionimg as $image)
-						<div class="corp-info__images__img">
-							<img src="{{$image['image']}}" alt="Section Image" class="corp-image" />
-						</div>
-					@endforeach
-				</div>
-				<div class="corp-info__text margin">
-					<h2 class="h1 primary-color">{{$title}}</h2>
-					<h2 class="h2 subtitle-color">{{$subtitle}}</h2>
-					<h3 class="h3 corp-info__text--left">{!!$description!!}</h3>
-				</div>
+		<?php 
+		$title = $block->getTitle();
+		$subtitle = $block->getSubtitle();
+		$description = $block->getDescription();
+		$sectionimg = $block->getSectionimg();
+		?>
+		<div class="corp-info">
+			<div class="corp-info__text">
+				<h2 class="h1-inner primary-color">{{$title}}</h2>
+				<h3 class="h2 subtitle">{{$subtitle}}</h3>
+				<p class="h2-inner corp-info__text corp-info__text--left ">{!!$description!!}</p>
+			</div>
+			<div class="corp-info__images">	
+				@foreach ($sectionimg as $image)
+					<div class="corp-info__images--image">
+						<img src="{{$image['image']}}" alt="Section Image" class="corp-image" />
+					</div>
+				@endforeach
 			</div>
 		</div>
 	</div>
