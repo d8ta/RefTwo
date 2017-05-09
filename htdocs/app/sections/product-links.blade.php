09a0bd24b2d1b0de7cf85733469bfca11b38114d
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-09 11:09:20 +0200

    position of text and logo changed; width of image changed according to feedback from scones

--- htdocs/app/sections/product-links.blade.php
+++ htdocs/app/sections/product-links.blade.php
@@ -14,10 +14,12 @@
 				?>
 				<a class="product-links__content" href="{{$link}}">
 					<img src="{{$batchimage}}" alt="Produkt Link" class="product-links__content__image--batch" />
-					<img src="{{$logo}}" alt="Batchspray Logo" class="product-links__content__image--logo" />
-					<h3 class="product-links__content__image__text h3>">
-						<div class="product-links__content__image__text__clamp">{{$description}}</div>
-					</h3>
+					<div class="product-links__content__image--logogroup">
+						<img src="{{$logo}}" alt="Batchspray Logo" class="product-links__content__image--logo" />
+						<h3 class="product-links__content__image__text h3>">
+							<div class="product-links__content__image__text__clamp">{{$description}}</div>
+						</h3>
+					</div>
 				</a>
 				@endforeach
 			</div>
