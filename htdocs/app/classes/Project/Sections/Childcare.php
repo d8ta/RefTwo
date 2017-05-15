7fa4be67d711570656a7a8f180738588e28a753a
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-15 07:55:02 +0200

    childcare section implemented

---
+++ htdocs/app/classes/Project/Sections/Childcare.php
@@ -0,0 +1,23 @@
+<?php
+namespace Project\Sections;
+
+use A365\Wordpress\Helpers\Acf\FieldHelper;
+
+class Childcare extends \A365\Wordpress\Block\AcfBlock {
+
+	protected $_template = 'Childcare';
+	protected static $_label = 'Kinderbetreuung';
+
+	/**
+	 * @inheritdoc
+	 */
+	public static function getAcfSubfields()
+	{
+		return [
+				FieldHelper::createImageCrop('background', 'Hintergrund', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'yes', 'save_format' => 'url', "width" => 1920, "height" => 800])->setRequired(),
+				FieldHelper::createText('title', 'Überschrift')->setRequired(),
+				FieldHelper::createText('subtitle', 'Unterüberschrift')->setRequired(),
+				FieldHelper::createWYSIWYG('description', 'Beschreibung')->setRequired(),	
+		];
+	}
+}
---
+++ htdocs/app/sections/childcare.blade.php
@@ -0,0 +1,32 @@
+<div class="section section--third">
+	<div class="section section--fullwidth">
+		<div class="section__content">
+			<div class="childcare">
+				<?php 
+				$background = $block->getBackground();
+				$title = $block->getTitle();
+				$subtitle = $block->getSubtitle();
+				$description = $block->getDescription();
+				?>
+
+				{{-- background img --}}
+				<div class="childcare__image bg-image" style="background-image: url({{$background}})">
+				</div>
+
+				{{-- textbos --}}
+				<div class="childcare__box">
+					<div class="childcare__box__text">
+						<h2 class="h1-inner primary-color childcare__box__text__title">{{$title}}</h2>
+						<h3 class="h2 subtitle childcare__box__text__subtitle">{{$subtitle}}</h3>
+							
+						<div class="childcare__box__text__description">
+							<p class="childcare__box__text__description__clamp h3">{!!$description!!}</p>
+						</div>
+
+					</div>	
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
