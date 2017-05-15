9febb4b860aec5278ae60e1837d2f687446ad6c1
Daniel Lehner <daniel.lehner@a365.at>
2017-05-15 18:41:24 +0200

    welcome slider

--- htdocs/app/sections/welcome-slider.blade.php
+++ htdocs/app/sections/welcome-slider.blade.php
@@ -11,25 +11,29 @@
 					$pretitle = $slide['pretitle'];
 					$description = $slide['description'];
 					$buttontext = $slide['button_text'];
+					$button_url = $slide['button_url'];
 					?>
 					<div class="welcome-slider__slide">
+						<div class="welcome-slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
+						</div>
 						<div class="welcome-slider__slide__inner">
-							<div class="welcome-slider__slide__inner__image bg-image" style="background-image: url({{$slide['background']}})">
-							</div>
-							<div class="welcome-slider__slide__inner__text section__content">
-								<div class="welcome-slider__slide__inner__text__table">
-									<div class="welcome-slider__slide__inner__text__table__td">
-										<h1 class="h5 welcome-slider__slide__inner__text__pretitle">{{$pretitle}}</h1>
-										<h2 class="h1 welcome-slider__slide__inner__text__title">{{$title}}</h2>
-										<p class="h2 welcome-slider__slide__inner__text__description">{{$description}}</p>
-										<button class="welcome-slider__slide__inner__text__table__td btn--icon-text" type="button">
+							<div class="welcome-slider__slide__text">
+								<div class="welcome-slider__slide__text__table">
+									<div class="welcome-slider__slide__text__td">
+										<div class="welcome-slider__slide__text__inner">
+											<h1 class="welcome-slider__slide__text__pretitle">{{$pretitle}}</h1>
+											<h2 class="welcome-slider__slide__text__title">{{$title}}</h2>
+											<p class="welcome-slider__slide__text__description">{{$description}}</p>
+										</div>
+										<a class="btn btn--icon-text" href="{{$button_url}}">
 											<i class="btn__icon primary-color"></i>
 											<span class="btn__text">{{$buttontext}}</span>
-										</button>
+										</a>
 									</div>
 								</div>
 							</div>
 						</div>
+						
 					</div>							
 					@endforeach
 			</div>
