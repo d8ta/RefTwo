3a297cfa094d3721be1639ec8830019b123ddee6
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-15 18:42:41 +0200

    text and images are now in one slider

--- htdocs/app/sections/childcare.blade.php
+++ htdocs/app/sections/childcare.blade.php
@@ -3,7 +3,7 @@
 ?>
 <div class="section section--third">
 	<div class="section section--fullwidth">
-		<div class="section__content">
+		<div class="section__content">	
  			<div class="childcare @if(count($slides) > 1) js-owl-carousel owl-carousel @endif">
 				@foreach ($slides as $slide)
 				<?php 
@@ -12,22 +12,22 @@
 				$subtitle = $slide['subtitle'];
 				$description = $slide['description'];
 				?>
+				<div>
+					{{-- background img --}}
+					<div class="childcare__image bg-image" style="background-image: url({{$background}})">
+					</div>
+					{{-- textbox --}}
+					<div class="childcare__box">
+						<div class="childcare__box__text">
+							<h2 class="h1-inner primary-color childcare__box__text__title">{{$title}}</h2>
+							<h3 class="h2 subtitle childcare__box__text__subtitle">{{$subtitle}}</h3>
+								
+							<div class="childcare__box__text__description">
+								<p class="childcare__box__text__description__clamp h3">{!!$description!!}</p>
+							</div>
 
-				{{-- background img --}}
-				<div class="childcare__image bg-image" style="background-image: url({{$background}})">
-				</div>
-
-				{{-- textbos --}}
-				<div class="childcare__box">
-					<div class="childcare__box__text">
-						<h2 class="h1-inner primary-color childcare__box__text__title">{{$title}}</h2>
-						<h3 class="h2 subtitle childcare__box__text__subtitle">{{$subtitle}}</h3>
-							
-						<div class="childcare__box__text__description">
-							<p class="childcare__box__text__description__clamp h3">{!!$description!!}</p>
-						</div>
-
-					</div>	
+						</div>	
+					</div>
 				</div>
 				@endforeach
 			</div>
