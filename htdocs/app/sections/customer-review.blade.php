8b853a72b9c8fd7bca9cf94f7955c172a82e0bd9
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-16 08:56:44 +0200

    reset section

--- htdocs/app/sections/customer-review.blade.php
+++ htdocs/app/sections/customer-review.blade.php
@@ -1,7 +1,7 @@
 <?php 
 $customerimg = $block->getCustomerImg();
-$slides = $block->getSlides();
-var_dump($slides)
+// $slides = $block->getSlides();
+// var_dump($slides);
 ?>
 
 <div class="section--fifth section--yellow">
@@ -9,14 +9,15 @@ var_dump($slides)
 	<div class="section section__content">
 		<div class="section__inner">
 			<div class="customer-review @if(count($slides) > 1) js-owl-carousel owl-carousel @endif">
-				@foreach ($slides as $slide)
+				{{-- @foreach ($slides as $slide) --}}
 				<?php 
-				$bigimage = $slide['big_image'];
-				$title = $slide['title'];
-				$description = $slide['description'];
-				$signature = $slide['signature'];
+				$bigimage = $block->getBigImage();
+				$title = $block->getTitle();
+				$description = $block->getDescription();
+				$signature = $block->getSignature();
+				// $customerimg = $block->getCustomerImage();
 				?>
-				<div>
+				{{-- <div> --}}
 					{{-- left --}}
 					<div class="customer-review__content customer-review__content--left js-matchheight" >
 						<h2 class="h1-inner">{{$title}}</h2>
@@ -28,7 +29,7 @@ var_dump($slides)
 						<div class="customer-review__content__image-container">
 							<div class="customer-review__content__images customer-review__content__images--big">
 								<img alt="{{$title}}" src="{{$bigimage}}" class="customer-review__content__images__image">
-								@foreach ($customerimg as $img)
+								@foreach ($customerimg as $img )
 							</div>
 							<div class="customer-review__content__images customer-review__content__images--small">
 								<?php 
@@ -39,8 +40,8 @@ var_dump($slides)
 							</div>
 						</div>	
 					</div>
-				</div>
-				@endforeach
+				{{-- </div> --}}
+				{{-- @endforeach --}}
 			</div>
 		</div>
 	</div>
