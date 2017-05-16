2f7fb55f2a0e13b4cf1f81eee61bd1582482ab7c
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-16 15:44:26 +0200

    image section ready WIP: desktop from xl

--- htdocs/app/sections/core-values.blade.php
+++ htdocs/app/sections/core-values.blade.php
@@ -3,23 +3,21 @@ $corevalues = $block->getCorevalues();
 ?>
 <div class="section section--second-corp">
 	<div class="section__content">
-		{{-- <div class="margin"> --}}
-			<div class="core-values">
-				@foreach ($corevalues as $value)
-				<?php 
-				$image = $value['image'];
-				$title = $value['title'];
-				$description = $value['description'];
-				?>
-				<div class="core-values__info">
-					<img src="{{$image}}" alt="Section Image" class="core-values__info__image" />
-					<div class="core-values__info__text">
-						<h2 class="h4 primary-color core-values__info__text core-values__info__text--title">{{$title}}</h2>
-						<div class="core-values__info__text core-values__info__text--description editor-content">{!!$description!!}</div>
-					</div>
+		<div class="core-values">
+			@foreach ($corevalues as $value)
+			<?php 
+			$image = $value['image'];
+			$title = $value['title'];
+			$description = $value['description'];
+			?>
+			<div class="core-values__info">
+				<img src="{{$image}}" alt="Section Image" class="core-values__info__image" />
+				<div class="core-values__info__text">
+					<h2 class="h4 primary-color core-values__info__text core-values__info__text--title">{{$title}}</h2>
+					<div class="core-values__info__text core-values__info__text--description editor-content">{!!$description!!}</div>
 				</div>
-				@endforeach
 			</div>
-		{{-- </div> --}}
+			@endforeach
+		</div>
 	</div>
 </div>
---
+++ htdocs/app/sections/images-section.blade.php
@@ -0,0 +1,26 @@
+<div class="section section--images-section section--first">
+	<div class="section__content">
+		<?php 
+		$image1 = $block->getImage1();
+		$image2 = $block->getImage2();
+		$image3 = $block->getImage3();
+		$image4 = $block->getImage4();
+		?>
+		<div class="images-section">
+
+			<div class="images-section__images">
+				<!-- left -->
+				<div class="images-section__images__left">
+					<img src="{{$image1}}" alt="Image" class="images-section__images__image" />
+					<img src="{{$image2}}" alt="Image" class="images-section__images__image" />
+				</div>
+				<!-- right -->
+				<div class="images-section__images__right">
+					<img src="{{$image3}}" alt="Image" class="images-section__images__image" />
+					<img src="{{$image4}}" alt="Image" class="images-section__images__image" />
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
