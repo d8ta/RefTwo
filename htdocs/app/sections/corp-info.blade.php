cb404eba4ae280b3b53f51c310d85c59909d30f1
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-16 17:19:56 +0200

    fixing fonts for first two sections

--- htdocs/app/sections/corp-info.blade.php
+++ htdocs/app/sections/corp-info.blade.php
@@ -10,16 +10,16 @@
 
 			{{-- left --}}
 			<div class="corp-info__text">
-				<h2 class="primary-color corp-info__text--title h1">{{$title}}</h2>
-				<h3 class="corp-info__text--subtitle h2 subtitle">{{$subtitle}}</h3>
-				<p class="corp-info__text corp-info__text--left corp-info__text--description h2">{!!$description!!}</p>
+				<h2 class="primary-color corp-info__text__title">{{$title}}</h2>
+				<h3 class="corp-info__text__subtitle">{{$subtitle}}</h3>
+				<div class="corp-info__text corp-info__text--left corp-info__text__description">{!!$description!!}</div>
 			</div>
 
 			{{-- right --}}
 			<div class="corp-info__images">	
 				@foreach ($sectionimg as $image)
-					<div class="corp-info__images--image">
-						<img src="{{$image['image']}}" alt="Section Image" class="corp-info__images--image" />
+					<div class="corp-info__images__image">
+						<img src="{{$image['image']}}" alt="Section Image" class="corp-info__images__image" />
 					</div>
 				@endforeach
 			</div>
