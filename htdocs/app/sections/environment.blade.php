371bb163fcf5810012eaf36ac2293242778ddf20
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-16 19:10:27 +0200

    environment changed back

--- htdocs/app/sections/environment.blade.php
+++ htdocs/app/sections/environment.blade.php
@@ -2,27 +2,27 @@
 	<div class="section section--fullwidth">
 		<div class="section__content">
 			<div class="environment">
-				<?php
+				<?php 
 				$background = $block->getBackground();
 				$title = $block->getTitle();
 				$subtitle = $block->getSubtitle();
 				$description = $block->getDescription();
 				?>
 
-
-
-			{{-- textbos --}}
-			<div class="environment__box">
-				<div class="environment__box__text">
-					<h2 class="environment__box__text__title">{{$title}}</h2>
-					<h3 class="environment__box__text__subtitle">{{$subtitle}}</h3>
-					<div class="environment__box__text__description">{!!$description!!}</div>
+				{{-- background img --}}
+				<div class="environment__image bg-image" style="background-image: url({{$background}})">
 				</div>
-			</div>
 
+				{{-- textbos --}}
+				<div class="environment__box">
+					<div class="environment__box__text">
+						<h2 class="environment__box__text__title">{{$title}}</h2>
+						<h3 class="environment__box__text__subtitle">{{$subtitle}}</h3>
+						<div class="environment__box__text__description">{!!$description!!}</div>
+					</div>	
+				</div>
+
+			</div>
 		</div>
-	</div>
-	{{-- background img --}}
-	<div class="environment__image bg-image" style="background-image: url({{$background}})">
 	</div>
 </div>
