6334103d5e7b6f1af8cca2b7560a07be00b1694c
Daniel Lehner <daniel.lehner@a365.at>
2017-05-17 16:49:22 +0200

    map repeater deleted

--- htdocs/app/classes/Project/Sections/ContactMap.php
+++ htdocs/app/classes/Project/Sections/ContactMap.php
@@ -15,34 +15,6 @@ class ContactMap extends \A365\Wordpress\Block\AcfBlock {
 	{
 		return [
 			FieldHelper::createText('headline', 'Überschrift')->setRequired(),
-			FieldHelper::createRepeater('maps', 'Map', ["min" => 1, "max" => 10])
-				->addSubfields([
-					// fieldgroup Google Maps
-					array (
-						'label' => 'Google Maps',
-						'name' => 'Google Maps',
-						'type' => 'google_map',
-						'instructions' => '',
-						'required' => 0,
-						'conditional_logic' => 0,
-						'wrapper' => array (
-							'width' => '',
-							'class' => '',
-							'id' => '',
-						),
-						'center_lat' => '',
-						'center_lng' => '',
-						'zoom' => '',
-						'height' => '',
-					),					
-					FieldHelper::createText('title', 'Überschrift')->setRequired(),
-					FieldHelper::createText('address', 'Adresse')->setRequired(),
-					FieldHelper::createText('phone', 'Telefon')->setRequired(),
-					FieldHelper::createText('fax', 'Fax')->setRequired(),
-					FieldHelper::createText('country', 'Länderauswahl')->setRequired(),
-					FieldHelper::createPageLink('button_url', 'Button Link'),
-					FieldHelper::createText('button_text', 'Button Text')->setRequired(),
-			]),
 		];
 	}
 }
