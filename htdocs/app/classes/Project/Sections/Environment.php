5c17805ff355a82561f687b85c0b6b9435cb2cc6
Daniel Lehner <daniel.lehner@a365.at>
2017-05-17 16:19:02 +0200

    environment + childcare in one

--- htdocs/app/classes/Project/Sections/Environment.php
+++ htdocs/app/classes/Project/Sections/Environment.php
@@ -6,7 +6,7 @@ use A365\Wordpress\Helpers\Acf\FieldHelper;
 class Environment extends \A365\Wordpress\Block\AcfBlock {
 
 	protected $_template = 'environment';
-	protected static $_label = 'Fullwidth Hintergrund, Textbox rechts';
+	protected static $_label = 'Slider: Fullwidth Hintergrund + Textbox';
 
 	/**
 	 * @inheritdoc
@@ -14,10 +14,14 @@ class Environment extends \A365\Wordpress\Block\AcfBlock {
 	public static function getAcfSubfields()
 	{
 		return [
+			FieldHelper::createRepeater('slides', 'Slides', ["min" => 1, "max" => 6])
+			->addSubfields([
 				FieldHelper::createImageCrop('background', 'Hintergrund', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 1920, "height" => 800])->setRequired(),
 				FieldHelper::createText('title', 'Überschrift')->setRequired(),
 				FieldHelper::createText('subtitle', 'Unterüberschrift')->setRequired(),
-				FieldHelper::createWYSIWYG('description', 'Beschreibung')->setRequired(),	
+				FieldHelper::createWYSIWYG('description', 'Beschreibung')->setRequired(),
+				FieldHelper::createTrueFalse('textright', 'Text rechts'),
+			])	
 		];
 	}
 }
--- htdocs/app/sections/environment.blade.php
+++ htdocs/app/sections/environment.blade.php
@@ -1,21 +1,29 @@
+<?php 
+	$slides = $block->getSlides();
+?>
 <div class="section section--fullwidth section--margin">
 	<div class="section__content">
-		<div class="environment">
-			<?php 
-			$background = $block->getBackground();
-			$title = $block->getTitle();
-			$subtitle = $block->getSubtitle();
-			$description = $block->getDescription();
-			?>
-			<div class="environment__image bg-image" style="background-image: url({{$background}})">
-			</div>
-			<div class="environment__box">
-				<div class="environment__box__text">
-					<h2 class="environment__box__text__title">{{$title}}</h2>
-					<h3 class="environment__box__text__subtitle">{{$subtitle}}</h3>
-					<div class="environment__box__text__description">{!!$description!!}</div>
-				</div>	
-			</div>
+		<div class="environment @if(count($slides) > 1) js-owl-carousel owl-carousel @endif">
+			@foreach ($slides as $slide)
+				<?php 
+					$background = $slide['background'];
+					$title = $slide['title'];
+					$subtitle = $slide['subtitle'];
+					$description = $slide['description'];
+					$textright = $slide['textright'];
+					?>
+				<div class="environment__slide @if($textright) environment__slide--text-right @endif">
+					<div class="environment__image bg-image" style="background-image: url({{$background}})">
+					</div>
+					<div class="environment__box">
+						<div class="environment__box__text">
+							<h2 class="environment__box__text__title">{{$title}}</h2>
+							<h3 class="environment__box__text__subtitle">{{$subtitle}}</h3>
+							<div class="environment__box__text__description">{!!$description!!}</div>
+						</div>	
+					</div>
+				</div>
+			@endforeach
 		</div>
 	</div>
 </div>
