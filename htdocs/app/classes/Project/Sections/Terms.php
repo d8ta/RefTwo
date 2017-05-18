2b82380a7b4d11ae8f48c7f719ae6bc7ae53b267
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-18 15:18:27 +0200

    simplify terms section

--- htdocs/app/classes/Project/Sections/Terms.php
+++ htdocs/app/classes/Project/Sections/Terms.php
@@ -6,20 +6,16 @@ use A365\Wordpress\Helpers\Acf\FieldHelper;
 class Terms extends \A365\Wordpress\Block\AcfBlock {
 
 	protected $_template = 'terms';
-	protected static $_label = '4  x Titel + Text';
+	protected static $_label = 'editor';
 	/**
 	 * @inheritdoc
 	 */
 	public static function getAcfSubfields()
 	{
 		return [
-				FieldHelper::createText('ceo', 'Name CEO')->setRequired(), 	
-				FieldHelper::createText('president', 'Name Präsident')->setRequired(), 	
-				FieldHelper::createText('companyname', 'Gmbh Name')->setRequired(), 
-				FieldHelper::createWYSIWYG('companyaddress', 'Adresse/VAT/Legal')->setRequired(), 
-				FieldHelper::createWYSIWYG('bankdata', 'Bankdaten')->setRequired(), 
-				FieldHelper::createPageLink('button_url', 'Button Link'),
-				FieldHelper::createText('button_text', 'Button Text')->setRequired(),
+				FieldHelper::createText('title', 'Titel')->setRequired(), 	
+				FieldHelper::createWYSIWYG('left', 'Editor links')->setRequired(), 
+				FieldHelper::createWYSIWYG('right', 'Editor rechts')->setRequired(), 
 		];
 	}
 }
