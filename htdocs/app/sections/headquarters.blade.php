42cb163b8f1849843d2fe0f3938a6eef5f5032ca
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-18 10:47:03 +0200

    responsive boxes for offices in contacts above the form

--- htdocs/app/sections/headquarters.blade.php
+++ htdocs/app/sections/headquarters.blade.php
@@ -7,30 +7,24 @@ $locations = \Project\Models\Location::allPublished();
 		<h1 class="headquarters__headline">Siconnex Standorte</h1>
 		<div class="headquarters__locations">
 			@foreach($locations as $location)
-				<div class="headquarters__locations__location" data-locationid="{{ $location->getId() }}">
-				
-					{{-- @include("components.location.address") --}}
-
 					@if (isset($location))
-					<address class="location-address">
-						<div class="location-address__group">
-							<span class="h2 location-address__headline">{{$location->getTitle()}}</span>
+
+					<address class="headquarters__locations__location" data-locationid="{{ $location->getId() }}">
+						<div class="headquarters__locations__location__group">
+							<span class="headquarters__locations__location__headline">{{$location->getTitle()}}</span>
 						</div>
-						<div class="location-address__group location-address__text">
+						<div class="headquarters__locations__location__group headquarters__locations__location__text">
 							<span>{!!$location->getStreet()!!}</span><br>
-							<span class="location-address__zip">{{$location->getZip()}}</span> <span>{{$location->getCity()}}</span><br>
+							<span class="headquarters__locations__location__zip">{{$location->getZip()}}</span> <span>{{$location->getCity()}}</span><br>
 							<span>{{$location->getCountry()}}</span>
 						</div>
-						<div class="location-address__group location-address__text">
+						<div class="headquarters__locations__location__group headquarters__locations__location__text">
 							<span>T: {{$location->getTel()}}</span><br>
 							E: <a href="mailto:{{$location->getEmail()}}">{{$location->getEmail()}}</a>
 						</div>
 					</address>
 					@endif
-
-				</div>
 			@endforeach
-		</div>
-				
+		</div>	
 	</div>
 </div>
