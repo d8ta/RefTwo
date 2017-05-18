b17838efb2456e99e4d6bdc29cbf2d9c27c8b940
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-18 12:11:00 +0200

    text formation ready

--- htdocs/app/sections/terms.blade.php
+++ htdocs/app/sections/terms.blade.php
@@ -13,18 +13,18 @@ $button_text = $block->getButtonText();
 			<div class="terms__info">
 				<div class="terms__info__text">
 
-					<h2 class="terms__infnfo__text terms__info__text__manager-title">Management</h2>
-					<div class="terms__info__text terms__info__text__manager-ceo">{{$ceo}}</div>
-					<div class="terms__info__text terms__info__text__manager-president">{{$president}}</div>
+					<h2 class="terms__infnfo__text terms__info__text__title">Management</h2>
+					<div class="terms__info__text terms__info__text__name">{{$ceo}}</div>
+					<div class="terms__info__text terms__info__text__name">{{$president}}</div>
 
-					<h2 class="terms__infnfo__text terms__info__text__address-title">Adresse</h2>
-					<div class="terms__info__text terms__info__text__address-companyname">{{$companyname}}</div>
-					<div class="terms__info__text terms__info__text__address-companyaddress editor-content">{!!$companyaddress!!}</div>
+					<h2 class="terms__infnfo__text terms__info__text__title">Adresse</h2>
+					<div class="terms__info__text terms__info__text__subtitle">{{$companyname}}</div>
+					<div class="terms__info__text terms__info__text__name editor-content">{!!$companyaddress!!}</div>
 					
-					<h2 class="terms__infnfo__text terms__info__text__bank-title">Bankdetails</h2>
+					<h2 class="terms__infnfo__text terms__info__text__title">Bankdetails</h2>
 					<div class="terms__info__text terms__info__text__bank-data editor-content">{!!$bankdata!!}</div>					
 					
-					<h2 class="terms__infnfo__text terms__info__text__legal-title">Geschäftsbedingungen</h2>
+					<h2 class="terms__infnfo__text terms__info__text__title">Geschäftsbedingungen</h2>
 					<a class="btn" href="{{$button_url}}">
 						{{$button_text}}
 					</a>
