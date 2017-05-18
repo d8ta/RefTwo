cb4a7b57c94f6fe9d0e70ceb9779f5bed13ca323
Daniel Lehner <daniel.lehner@a365.at>
2017-05-18 15:43:00 +0200

    submenu updated

--- htdocs/app/views/components/navigation/submenu-21.blade.php
+++ htdocs/app/views/components/navigation/submenu-21.blade.php
@@ -1,15 +1,18 @@
 <?php
-$page_id = 21;
+$title = __('Siconnex');
+$description = __('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id praesentium eum.');
+$linktext = __('Mehr erfahren');
+$link_page_id = 21;
 ?>
 <div class="nav-content">
-	<a class="nav-content__image-box" href="{{get_permalink(pll_get_post($page_id))}}">
+	<a class="nav-content__image-box" href="{{get_permalink(pll_get_post($link_page_id))}}">
 		<div class="nav-content__image-box__image">
 			<div class="nav-content__image-box__image__img bg-image" style="background-image: url('assets/images/nav-menu/siconnex.jpg')"></div>
 		</div>
 		<div class="nav-content__image-box__text">
-			<div class="nav-content__image-box__text__headline"><?php _e('Siconnex'); ?></div>
-			<div class="nav-content__image-box__text__description"><?php _e('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id praesentium eum.'); ?></div>
-			<div class="nav-content__image-box__text__link"><?php _e('Mehr erfahren'); ?></div>
+			<div class="nav-content__image-box__text__headline">{{ $title }}</div>
+			<div class="nav-content__image-box__text__description">{{ $description }}</div>
+			<div class="nav-content__image-box__text__link">{{ $linktext }}</div>
 		</div>
 	</a>
 </div>
