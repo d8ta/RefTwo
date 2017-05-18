eadbf7fb0d63997c606d2881b45b23e11c290ac5
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-18 13:56:50 +0200

    deleting whitespace in terms.blade

--- htdocs/app/sections/terms.blade.php
+++ htdocs/app/sections/terms.blade.php
@@ -12,26 +12,19 @@ $button_text = $block->getButtonText();
 		<div class="terms">
 			<div class="terms__info">
 				<div class="terms__info__text">
-
-
 					<h2 class="terms__infnfo__text terms__info__text__headline">Impressum</h2>
-
 					<h2 class="terms__infnfo__text terms__info__text__title">Management</h2>
 					<div class="terms__info__text terms__info__text__name">{{$ceo}}</div>
 					<div class="terms__info__text terms__info__text__name">{{$president}}</div>
-
 					<h2 class="terms__infnfo__text terms__info__text__title">Adresse</h2>
 					<div class="terms__info__text terms__info__text__subtitle">{{$companyname}}</div>
 					<div class="terms__info__text terms__info__text__name editor-content">{!!$companyaddress!!}</div>
-					
 					<h2 class="terms__infnfo__text terms__info__text__title">Bankdetails</h2>
 					<div class="terms__info__text terms__info__text__bank-data editor-content">{!!$bankdata!!}</div>					
-					
 					<h2 class="terms__infnfo__text terms__info__text__title">Geschäftsbedingungen</h2>
 					<a class="btn" href="{{$button_url}}">
 						{{$button_text}}
 					</a>
-
 				</div>
 			</div>
 		</div>
