b476eca5dbce094b9fb0431d983db2389fee1712
Daniel Lehner <daniel.lehner@a365.at>
2017-05-23 11:10:35 +0200

    bugfix news

--- htdocs/app/views/posts/news.blade.php
+++ htdocs/app/views/posts/news.blade.php
@@ -4,13 +4,11 @@ $blocks = Project\Application::getInstance()->getConfig('posts.news.blocks');
 @extends('layouts.master')
 
 @section('content')
-	<div class="theme--product">
-		@foreach($blocks as $class)
-			<?php
-			$block = new $class;
-			$block->setNews($news);
-			?>
-			{!!$block->toHtml()!!}
-		@endforeach
-	</div>
+	@foreach($blocks as $class)
+		<?php
+		$block = new $class;
+		$block->setNews($news);
+		?>
+		{!!$block->toHtml()!!}
+	@endforeach
 @endsection
