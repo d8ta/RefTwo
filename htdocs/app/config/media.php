0ba893297935adc5e85f3fe89154ceaea25015fb
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-23 17:52:02 +0200

    email with logo now

--- htdocs/app/config/media.php
+++ htdocs/app/config/media.php
@@ -1,12 +1,12 @@
 <?php
 $config = [
     "media" => [
-    	"logo" => "assets/images/logo/rieder-logo.svg",
         "imageSizes" => [
             'infobox' => [
                 'width'  => 600,
                 'height' => 450,
             ],
         ],
+        "logo" => "assets/images/layout/siconnex_logo.png",
     ],
 ];
