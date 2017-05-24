f3d924252a7fb133e2af13b36167083edd25aab3
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-24 15:28:45 +0200

    changing environment slider to table layout for better fitting texts

--- htdocs/app/sections/environment.blade.php
+++ htdocs/app/sections/environment.blade.php
@@ -16,11 +16,15 @@
 					<div class="environment__image bg-image" style="background-image: url({{$background}})">
 					</div>
 					<div class="environment__box">
-						<div class="environment__box__text">
-							<h2 class="environment__box__text__title">{{$title}}</h2>
-							<h3 class="environment__box__text__subtitle">{{$subtitle}}</h3>
-							<div class="environment__box__text__description">{!!$description!!}</div>
-						</div>	
+						<div class="table table--fullheight">
+							<div class="table__td">
+								<div class="environment__box__text">
+									<h2 class="environment__box__text__title">{{$title}}</h2>
+									<h3 class="environment__box__text__subtitle">{{$subtitle}}</h3>
+									<div class="environment__box__text__description">{!!$description!!}</div>
+								</div>	
+							</div>
+						</div>
 					</div>
 				</div>
 			@endforeach
