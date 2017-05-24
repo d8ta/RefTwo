2ac389e106e3b390f0b6ca06fa405ef07945600e
Lina Tang <lina.tang@a365.at>
2017-05-24 14:26:43 +0200

    added functional timeline without animation

--- htdocs/app/sections/timeline.blade.php
+++ htdocs/app/sections/timeline.blade.php
@@ -8,7 +8,7 @@
 
 <div class="section section--yellow section--margin">
 	<div class="section__content">
-		<div class="timeline">
+		<div class="timeline js-timeline">
 			<div class="timeline__wrapper">
 				<h2 class="h2">{{$headline}}</h2>
 				<div class="timeline__intro">
@@ -19,7 +19,7 @@
 				<div class="timeline__horizontal__line">
 					<div class="timeline__horizontal__line__content table">
 						@foreach($timeline as $key => $elem)
-						<div class="timeline__horizontal__line__content__inner table__td table__td--center @if($key === $rand_active) active @endif" data-id="id-{{$key}}">
+						<div class="timeline__horizontal__line__content__inner table__td table__td--center js-timeline__nav @if($key === $rand_active) active @endif" data-id="{{$key}}">
 							<div class="timeline__horizontal__line__content__inner__image">
 								<img src="{{$elem['image']}}" alt="{{$elem['title']}}">
 							</div>
@@ -33,7 +33,7 @@
 				</div> 
 				<div class="timeline__horizontal__content">
 					@foreach($timeline as $key => $elem)
-						<div class="timeline__horizontal__content__text @if($key === $rand_active) active @endif" data-id="id-{{$key}}">
+						<div class="timeline__horizontal__content__text js-timeline__content @if($key === $rand_active) active @endif" data-id="{{$key}}">
 							<div class="timeline__horizontal__content__inner timeline__horizontal__content__inner--left">
 								<h2 class="h2">{{$elem['title']}}</h2>
 							</div>
