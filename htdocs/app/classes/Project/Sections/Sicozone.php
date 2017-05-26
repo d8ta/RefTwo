1c700a9f9887c0f274fa127a616aa1dcecfaf2be
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-26 11:07:53 +0200

    changint sic ozone section completely for fitting the folder info about it

--- htdocs/app/classes/Project/Sections/Sicozone.php
+++ htdocs/app/classes/Project/Sections/Sicozone.php
@@ -6,7 +6,7 @@ use A365\Wordpress\Helpers\Acf\FieldHelper;
 class Sicozone extends \A365\Wordpress\Block\AcfBlock {
 
 	protected $_template = 'sicozone';
-	protected static $_label = 'Fullwidth Hintergrund + Textbox links';
+	protected static $_label = 'Title/Subtitle + Editor';
 
 	/**
 	 * @inheritdoc
@@ -14,7 +14,6 @@ class Sicozone extends \A365\Wordpress\Block\AcfBlock {
 	public static function getAcfSubfields()
 	{
 		return [
-				FieldHelper::createImageCrop('background', 'Hintergrund', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 1920, "height" => 800])->setRequired(),
 				FieldHelper::createText('title', 'Überschrift')->setRequired(),
 				FieldHelper::createText('subtitle', 'Unterüberschrift')->setRequired(),
 				FieldHelper::createWYSIWYG('description', 'Beschreibung')->setRequired(),	
--- htdocs/app/sections/sicozone.blade.php
+++ htdocs/app/sections/sicozone.blade.php
@@ -1,21 +1,16 @@
-	<div class="section section--fullwidth section--margin">
+	<div class="section section--margin">
 		<div class="section__content">
 			<div class="sicozone">
 				<?php 
-				$background = $block->getBackground();
 				$title = $block->getTitle();
 				$subtitle = $block->getSubtitle();
 				$description = $block->getDescription();
 				?>
-				<div class="sicozone__image bg-image" style="background-image: url({{$background}})">
-				</div>
-				<div class="sicozone__box">
-					<div class="sicozone__box__text">
-						<h2 class="sicozone__box__text__title">{{$title}}</h2>
-						<h3 class="sicozone__box__text__subtitle">{{$subtitle}}</h3>
-						<div class="sicozone__box__text__description">{!!$description!!}</div>
-					</div>	
-				</div>
+				<div class="sicozone__text">
+					<h2 class="sicozone__text__title">{{$title}}</h2>
+					<h3 class="sicozone__text__subtitle">{{$subtitle}}</h3>
+					<div class="sicozone__text__description editor-content">{!!$description!!}</div>
+				</div>	
 			</div>
 		</div>
 	</div>
