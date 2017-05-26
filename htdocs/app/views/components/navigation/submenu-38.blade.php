0be7b05287509d1a881b92efa68160e48b216e7b
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-26 12:26:23 +0200

    fix for images in subnav

--- htdocs/app/views/components/navigation/submenu-38.blade.php
+++ htdocs/app/views/components/navigation/submenu-38.blade.php
@@ -7,7 +7,7 @@ $link_page_id = 38;
 <div class="nav-content">
 	<a class="nav-content__image-box" href="{{get_permalink(pll_get_post($link_page_id))}}">
 		<div class="nav-content__image-box__image">
-			<div class="nav-content__image-box__image__img bg-image" style="background-image: url('assets/images/nav-menu/karriere.jpg') border-radius: .5em"></div>
+			<div class="nav-content__image-box__image__img bg-image" style="background-image: url('assets/images/nav-menu/karriere.jpg'); border-radius: .5em"></div>
 		</div>
 		<div class="nav-content__image-box__text">
 			<div class="nav-content__image-box__text__headline">{{ $title }}</div>
