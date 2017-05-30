93cbd39dbb58ea29b4e1f81bbfc15aadb15b082a
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-30 14:04:00 +0200

    image section rebuild ready

--- htdocs/app/sections/images-section.blade.php
+++ htdocs/app/sections/images-section.blade.php
@@ -7,19 +7,25 @@
 		$image4 = $block->getImage4();
 		?>
 		<div class="images-section">
-
 			<div class="images-section__images">
 				<!-- left -->
 				<div class="images-section__images__left">
-					<img src="{{$image1}}" alt="Image" class="images-section__images__image__left" />
-					<img src="{{$image2}}" alt="Image" class="images-section__images__image__left" />
+					<div class="images-section__images__left__img">
+						<img src="{{$image1}}" alt="Image" class="" />
+					</div>				
+					<div class="images-section__images__left__img">
+						<img src="{{$image2}}" alt="Image" class="" />
+					</div>
 				</div>
 				<!-- right -->
 				<div class="images-section__images__right">
-					<img src="{{$image3}}" alt="Image" class="images-section__images__image__right" />
-					<img src="{{$image4}}" alt="Image" class="images-section__images__image__right" />
+					<div class="images-section__images__right__img">
+						<img src="{{$image3}}" alt="Image" class="" />
+					</div>				
+					<div class="images-section__images__right__img">
+						<img src="{{$image4}}" alt="Image" class="" />
+					</div>
 				</div>
-
 			</div>
 		</div>
 	</div>
