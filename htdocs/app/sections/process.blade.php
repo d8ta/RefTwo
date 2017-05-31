431b6a61c0434a44a092c1651f19248fbf73fcff
Daniel Raudschus <daniel.raudschus@a365.at>
2017-05-31 17:04:34 +0200

    process section ready

--- htdocs/app/sections/process.blade.php
+++ htdocs/app/sections/process.blade.php
@@ -5,6 +5,7 @@ $title = $block->getTitle();
 <div class="section section--margin-xl">
 	<div class="section__content">
 		<div class="process">
+			<h2 class="process__info__text__title">{{$title}}</h2>
 			@foreach ($sections as $section)
 			<?php 
 			$image = $section['image'];
@@ -14,11 +15,10 @@ $title = $block->getTitle();
 			?>
 			<div class="process__info">
 				<div class="process__info__text">
-					<h2 class="process__info__text__title">{{$title}}</h2>
 					<img src="{{$image}}" alt="application Image" class="process__info__text__image" />
-					<div class="process__info__text process__info__text__editor editor-content">{!!$left!!}</div>		
+					<div class="process__info__text process__info__text__editor__left editor-content">{!!$left!!}</div>		
 					<div class="process__info__text process__info__text__editor__middle editor-content">{!!$middle!!}</div>
-					<div class="process__info__text process__info__text__editor editor-content">{!!$right!!}</div>
+					<div class="process__info__text process__info__text__editor__right editor-content">{!!$right!!}</div>
 				</div>
 			</div>
 			@endforeach
