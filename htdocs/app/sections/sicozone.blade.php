c3a5c119a5c0aff5bc3aede2a7d54208a33d2871
Daniel Raudschus <daniel.raudschus@a365.at>
2017-06-20 16:54:16 +0200

    sicozone style changed

--- htdocs/app/sections/sicozone.blade.php
+++ htdocs/app/sections/sicozone.blade.php
@@ -1,16 +1,25 @@
 	<div class="section section--margin">
 		<div class="section__content">
 			<div class="sicozone">
-				<?php 
-				$title = $block->getTitle();
-				$subtitle = $block->getSubtitle();
-				$description = $block->getDescription();
-				?>
+				<?php
+                $title = $block->getTitle();
+                $subtitle = $block->getSubtitle();
+                $description = $block->getDescription();
+                ?>
 				<div class="sicozone__text">
-					<h2 class="sicozone__text__title">{{$title}}</h2>
-					<h3 class="sicozone__text__subtitle">{{$subtitle}}</h3>
-					<div class="sicozone__text__description editor-content">{!!$description!!}</div>
-				</div>	
+
+					<!-- left -->
+					<div class="sicozone__text__left">
+						<h2 class="sicozone__text__left__title">{{$title}}</h2>
+						<h3 class="sicozone__text__left__subtitle">{{$subtitle}}</h3>
+					</div>
+
+					<!-- right -->
+					<div class="sicozone__text__right">
+						<div class="sicozone__text__right__description editor-content">{!!$description!!}</div>
+					</div>
+
+				</div>
 			</div>
 		</div>
 	</div>
