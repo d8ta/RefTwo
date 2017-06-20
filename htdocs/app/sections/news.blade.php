91f5fc5cf583c672b6aaaa1ad9a64986dbd7307f
Daniel Raudschus <daniel.raudschus@a365.at>
2017-06-20 14:49:38 +0200

    WIP: show all news from the $news_items array on the news site

--- htdocs/app/sections/news.blade.php
+++ htdocs/app/sections/news.blade.php
@@ -1,29 +1,28 @@
 <?php
 $news_items = Project\Models\News::allPublished();
-
-$news_item_src = $news_items[0];
-
-$image_id = $news_item_src->getHomeImage();
-$image = wp_get_attachment_image_src($image_id, 'full');
-
-$news_item = array();
-$news_item['title'] = $news_item_src->getPostTitle();
-$news_item['subtitle'] = $news_item_src->getSubline();
-$news_item['description'] = $news_item_src->getContent();
-$news_item['button_text'] = __("Mehr erfahren");
-$news_item['icon'] = "vorteile";
-$news_item['button_url'] = get_permalink(pll_get_post($news_item_src->getId()));
-$news_item['background'] = $image[0];
-$boxes[0] = $news_item;
 ?>
+
+@foreach ($news_items as $item)
+<?php
+    $image_id = $item->getHomeImage();
+    $image = wp_get_attachment_image_src($image_id, 'full');
+    $news_item = array();
+    $news_item['title'] = $item->getPostTitle();
+    $news_item['subtitle'] = $item->getSubline();
+    $news_item['description'] = $item->getContent();
+    $news_item['button_text'] = __("Mehr erfahren");
+    $news_item['icon'] = "vorteile";
+    $news_item['button_url'] = get_permalink(pll_get_post($item->getId()));
+    $news_item['background'] = $image[0];
+
+    $title = $news_item['title'];
+    $subtitle = $news_item['subtitle'];
+    $description = $news_item['description'];
+    $sectionimg = $news_item['background'];
+    ?>
+
 <div class="section section--news section--margin">
 	<div class="section__content">
-		<?php
-        $title = $news_item['title'];
-        $subtitle = $news_item['subtitle'];
-        $description = $news_item['description'];
-        $sectionimg = $news_item['background'];
-        ?>
 		<div class="news">
 
 			{{-- left --}}
@@ -43,3 +42,4 @@ $boxes[0] = $news_item;
 		</div>
 	</div>
 </div>
+@endforeach
