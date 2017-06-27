2e3d165e2dcf1cd2d17acbd8d0d332bc910c3477
Daniel Raudschus <daniel.raudschus@a365.at>
2017-06-27 09:53:55 +0200

    sicOzone new Layout

--- htdocs/app/sections/sicozone.blade.php
+++ htdocs/app/sections/sicozone.blade.php
@@ -7,18 +7,15 @@
                 $description = $block->getDescription();
                 ?>
 				<div class="sicozone__text">
-
 					<!-- left -->
 					<div class="sicozone__text__left">
 						<h2 class="sicozone__text__left__title">{{$title}}</h2>
 						<h3 class="sicozone__text__left__subtitle">{{$subtitle}}</h3>
 					</div>
-
 					<!-- right -->
 					<div class="sicozone__text__right">
 						<div class="sicozone__text__right__description editor-content">{!!$description!!}</div>
 					</div>
-
 				</div>
 			</div>
 		</div>
