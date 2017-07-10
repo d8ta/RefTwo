322ee844556573f4826bbffffffd5d7f62819460
Daniel Raudschus <daniel.raudschus@a365.at>
2017-07-10 11:17:12 +0200

    file beautified

--- htdocs/app/sections/batchspray.blade.php
+++ htdocs/app/sections/batchspray.blade.php
@@ -1,6 +1,6 @@
 <div class="section section--batchspray section--margin-xl">
 	<div class="section__content">
-		<?php 
+		<?php
 		$title = $block->getTitle();
 		$subtitle = $block->getSubtitle();
 		$description = $block->getDescription();
@@ -14,18 +14,18 @@
 
 			{{-- left --}}
 			<div class="batchspray__text">
-				<h2 class="batchspray__text__title">{{$title}}</h2>
+				<h2 class="batchspray__text__title">{!!$title!!}</h2>
 				<h3 class="batchspray__text__subtitle">{{$subtitle}}</h3>
 				<div class="batchspray__text__description editor-content">{!!$description!!}</div>
 				<div class="batchspray__text__footer">
 					<a class="btn btn--yellow" href="{{$button_url}}">
 						{{$buttontext}}
 					</a>
-					<img src="{{$logo}}" alt="Logo" class="batchspray__text__logo" />		
+					<img src="{{$logo}}" alt="Logo" class="batchspray__text__logo" />
 				</div>
 			{{-- right --}}
 			</div>
-			<div class="batchspray__images">	
+			<div class="batchspray__images">
 				<img src="{{$image}}" alt="Image" class="batchspray__images__image" />
 			</div>
 
