1b4bd8b362aa2f29433138bab2ec1338fb2027e4
Lina Tang <lina.tang@a365.at>
2017-07-18 12:47:45 +0200

    once again

--- htdocs/app/sections/technology-teaser.blade.php
+++ htdocs/app/sections/technology-teaser.blade.php
@@ -14,7 +14,7 @@
 			?>
 			<h2 class="technology-teaser__inner__title">{!!$title!!}</h2>
 			<h3 class="technology-teaser__inner__subtitle">{{$subtitle}}</h3>
-			<div class="technology-teaser__inner__description">{{$description}}</div>
+			<div class="technology-teaser__inner__description">{!!$description!!}</div>
 		</div>
 
 		<div class="technology-teaser__boxes">
