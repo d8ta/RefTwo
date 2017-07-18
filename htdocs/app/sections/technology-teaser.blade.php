e1608617ac6fdbb812ab47a3112bc7fb2816e23d
Lina Tang <lina.tang@a365.at>
2017-07-18 12:35:20 +0200

    escaped teaser

--- htdocs/app/sections/technology-teaser.blade.php
+++ htdocs/app/sections/technology-teaser.blade.php
@@ -12,7 +12,7 @@
 			$subtitle = $block->getSubtitle();
 			$description = $block->getDescription();
 			?>
-			<h2 class="technology-teaser__inner__title">{{$title}}</h2>
+			<h2 class="technology-teaser__inner__title">{!!$title!!}</h2>
 			<h3 class="technology-teaser__inner__subtitle">{{$subtitle}}</h3>
 			<div class="technology-teaser__inner__description">{{$description}}</div>
 		</div>
