110803e01b99f8b5bc5967dda3d985430b19d0a2
Lina Tang <lina.tang@a365.at>
2017-07-18 11:02:14 +0200

    changed image to gallery, changed frontend, changed hardcoded images

--- htdocs/app/views/posts/news/body.blade.php
+++ htdocs/app/views/posts/news/body.blade.php
@@ -2,9 +2,7 @@
 	<div class="section__content">
 		<?php 
 			$news = $block->getNews();
-			$image_id = $news->getHeaderImage();
-			$image = wp_get_attachment_image_src($image_id, 'full');
-			$image_meta = wp_get_attachment_metadata($image_id);
+			$images = $news->getImages();
 		?>
 		<div class="news">
 			<div class="news__text">
@@ -13,9 +11,9 @@
 				<div class="news__text__content editor-content">{!!$news->getContent()!!}</div>	
 			</div>
 			<div class="news__images">
-				<img src="assets/images/layout/newsimg1.png" alt="News Bild" class="responsive news__images__img" />	
-				<img src="assets/images/layout/newsimg2.png" alt="News Bild" class="responsive news__images__img" />	
-				<img data-src="{{$image[0]}}" alt="{{$imagge_meta['image_meta']['caption']}}" class="responsive news__images__img" />	
+				@foreach($images as $img)
+					<img src="{{$img['url']}}" alt="{{$news->getPostTitle()}}" class="responsive news__images__img" />	
+				@endforeach
 			</div>
 		</div>
 	</div>
