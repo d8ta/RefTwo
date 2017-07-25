ca490e7159e0b283a5cad3a21f07188c1bcf227d
Daniel Lehner <daniel.lehner@a365.at>
2017-07-25 11:57:03 +0200

    contact-page fixes

--- htdocs/app/sections/headquarters.blade.php
+++ htdocs/app/sections/headquarters.blade.php
@@ -9,14 +9,16 @@ $subline = $block->getSubline();
 		<h2 class="headquarters__headline">{{$headline}}</h2>
 		<h2 class="headquarters__subline">{{$subline}}</h2>
 		<div class="headquarters__locations">
+			<?php $first = true; ?>
 			@foreach($locations as $location)
-					@if (isset($location))
+				@if (isset($location))
 
 					<address class="headquarters__locations__location" data-locationid="{{ $location->getId() }}">
 						<div class="headquarters__locations__location__group">
 							<span class="headquarters__locations__location__headline">{{$location->getTitle()}}</span>
 						</div>
 						<div class="headquarters__locations__location__group headquarters__locations__location__text">
+							@if(strlen($location->getDescription()))<span>{!!$location->getDescription()!!}</span><br>@endif
 							<span>{!!$location->getStreet()!!}</span><br>
 							<span class="headquarters__locations__location__zip">{{$location->getZip()}}</span> <span>{{$location->getCity()}}</span><br>
 							<span>{{$location->getCountry()}}</span>
@@ -26,7 +28,11 @@ $subline = $block->getSubline();
 							E: <a href="mailto:{{$location->getEmail()}}">{{$location->getEmail()}}</a>
 						</div>
 					</address>
+					@if($first)
+						</div><div class="headquarters__locations">
 					@endif
+					<?php $first = false; ?>
+				@endif
 			@endforeach
 		</div>
 	</div>
--- htdocs/app/views/components/location/address.blade.php
+++ htdocs/app/views/components/location/address.blade.php
@@ -4,6 +4,7 @@
 			<span class="h2 location-address__headline">{{$location->getTitle()}}</span>
 		</div>
 		<div class="location-address__group location-address__text">
+			@if(strlen($location->getDescription()))<span>{!!$location->getDescription()!!}</span><br>@endif
 			<span>{!!$location->getStreet()!!}</span><br>
 			<span class="location-address__zip">{{$location->getZip()}}</span> <span>{{$location->getCity()}}</span><br>
 			<span>{{$location->getCountry()}}</span>
