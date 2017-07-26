b11e0d9e255c7abce90f876835f7cbb34ef1a135
Lina Tang <lina.tang@a365.at>
2017-07-26 14:56:24 +0200

    some stuffs

--- htdocs/app/classes/Project/Sections/CustomerReview.php
+++ htdocs/app/classes/Project/Sections/CustomerReview.php
@@ -14,17 +14,18 @@ class CustomerReview extends \A365\Wordpress\Block\AcfBlock
     public static function getAcfSubfields()
     {
         return [
-      FieldHelper::createRepeater('slides', 'Slides', ["min" => 1, "max" => 10])
+        FieldHelper::createTrueFalse('no_quote', 'keine Anführungszeichen'),
+        FieldHelper::createRepeater('slides', 'Slides', ["min" => 1, "max" => 10])
         ->addSubfields([
           FieldHelper::createText('title', 'Überschrift')->setRequired(),
           FieldHelper::createText('description', 'Beschreibung')->setRequired(),
-          FieldHelper::createText('signature', 'Unterschrift')->setRequired(),
+          FieldHelper::createText('signature', 'Unterschrift'),
         ]),
-                    FieldHelper::createImageCrop('big_image', 'Kundenbild groß', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 620, "height" => 620])->setRequired(),
-                    FieldHelper::createRepeater('customer_img', 'Kundenbilder', ["min" => 3, "max" => 3])
-                    ->addSubfields([
-                                    FieldHelper::createImageCrop('image', 'Kundenbild klein', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 200, "height" => 200])->setRequired()
-                                    ]),
-                ];
+        FieldHelper::createImageCrop('big_image', 'Kundenbild groß', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 620, "height" => 620])->setRequired(),
+        FieldHelper::createRepeater('customer_img', 'Kundenbilder', ["min" => 3, "max" => 3])
+        ->addSubfields([
+          FieldHelper::createImageCrop('image', 'Kundenbild klein', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 200, "height" => 200])
+          ]),
+        ];
     }
 }
--- htdocs/app/sections/customer-review.blade.php
+++ htdocs/app/sections/customer-review.blade.php
@@ -2,6 +2,8 @@
     $slides = $block->getSlides();
     $customerimg = $block->getCustomerImg();
     $bigimage = $block->getBigImage();
+    $noquote = $block->getNo_quote();
+    $rand = rand(5, 10000);
 ?>
 <div class="section section--margin-sm section--yellow">
 	<div class="section__content">
@@ -9,45 +11,47 @@
 			<div class="customer-review__whitebox-helper"></div>
 		</div>
 		<div class="customer-review">
-			<div class="customer-review__content customer-review__content--left js-matchheight">
-						<div class="customer-review__slider @if(count($slides) > 1) js-owl-carousel owl-carousel @endif">
-							@foreach ($slides as $slide)
-								<?php
-							    $title = $slide['title'];
-							    $description = $slide['description'];
-							    $signature = $slide['signature'];
-							    ?>
+			<div class="customer-review__content customer-review__content--left js-matchheight" data-mh="{{$rand}}">
+				<div class="customer-review__slider @if(count($slides) > 1) js-owl-carousel owl-carousel @endif">
+					@foreach ($slides as $slide)
+						<?php
+					    $title = $slide['title'];
+					    $description = $slide['description'];
+					    $signature = $slide['signature'];
+					    ?>
 
-							<div class="table table--fullheight">
-								<div class="table__td">
-								{{-- left --}}
-								<div class="customer-review__text customer-review__text--quote">
-									<div class="customer-review__text__inner">
-										<h2 class="customer-review__text__title">{{$title}}</h2>
-										<div class="customer-review__text__description">
-											{{$description}}
-										</div>
-										<div class="customer-review__text__author">{{$signature}}</div>
-									</div>
+					<div class="table table--fullheight">
+						<div class="table__td">
+						{{-- left --}}
+						<div class="customer-review__text customer-review__text--quote">
+							<div class="customer-review__text__inner">
+								<h2 class="customer-review__text__title">{!!$title!!}</h2>
+								<div class="customer-review__text__description editor-content editor-content--fake @if($noquote)customer-review__text__description--no-quote @endif">
+									{!!$description!!}
 								</div>
-								</div>
+								<div class="customer-review__text__author">{{$signature}}</div>
 							</div>
-							@endforeach
+						</div>
+						</div>
+					</div>
+					@endforeach
 				</div>
 			</div>
 			{{-- right --}}
-			<div class="customer-review__content customer-review__content--right js-matchheight">
+			<div class="customer-review__content customer-review__content--right js-matchheight" data-mh={{$rand}}">
 				<div class="table table--fullheight">
 					<div class="table__td">
 						<div class="customer-review__images">
 							<div class="customer-review__images__big">
 								<img src="{{$bigimage}}" class="customer-review__images__img" alt="{{$title}}">
+							</div>
 								@foreach ($customerimg as $img )
-							</div>
-							<div class="customer-review__images__small">
-								<img src="{{$img['image']}}" class="customer-review__images__img" alt="{{$title}}">
+									@if($img['image'])
+									<div class="customer-review__images__small">
+										<img src="{{$img['image']}}" class="customer-review__images__img" alt="{{$title}}">
+									</div>
+									@endif
 								@endforeach
-							</div>
 						</div>
 					</div>
 				</div>
