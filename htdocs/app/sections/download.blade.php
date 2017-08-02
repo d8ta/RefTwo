43f42e6c3550e1b8be5c7cf489fd326de0b95d3c
Daniel Lehner <daniel.lehner@a365.at>
2017-08-02 16:18:01 +0200

    download section bugfixes

--- htdocs/app/sections/download.blade.php
+++ htdocs/app/sections/download.blade.php
@@ -10,15 +10,15 @@
 	<div class="section__content">
 		<div class="download">
 		 	<div class="download__wrapper">
-		 		<h2 class="h2">{{$headline}}</h2>
-		 		<h2 class="h3">{{$subline}}</h3>
+		 		<h2 class="h1">{{$headline}}</h2>
+		 		<h3 class="h2">{{$subline}}</h3>
 		 		<div class="download__content">
 		 			<div class="download__list">
 						@foreach($downloads as $download)
 		 					<a class="download__list__elem" target="_blank" href="{{$download['file']['url']}}">
 		 						<div class="download__list__elem__content download__list__elem__content--info ">
-		 							<h2 class="h2">{{$download["title"]}}</h2>
-		 							<p>{{$download["info"]}}<p>
+		 							<h4 class="download__list__elem__content__headline h4">{{$download["title"]}}</h4>
+		 							<div class="download__list__elem__content__text">{{$download["info"]}}</div>
 		 						</div>
 		 						<div class="download__list__elem__content download__list__elem__content--link ">
 		 							<div class="table table--fullheight">
