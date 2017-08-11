e8474eb9efd953f48e5043fa8f862b35eb93fd79
Daniel Lehner <daniel.lehner@a365.at>
2017-08-11 12:20:54 +0200

    fallback geoip

--- htdocs/geoip.php
+++ htdocs/geoip.php
@@ -21,9 +21,13 @@ $gi = geoip_open(__DIR__ . "/GeoIPCity.dat", GEOIP_STANDARD);
 
 $record = geoip_record_by_addr($gi, $ip);
 
-$country_code = strtolower($record->country_code);
-
-setcookie('country_code', $country_code, time() + 3600 * 24, '/');
-
-setcookie('lat_ip', $record->latitude, time() + 3600 * 24, '/');
-setcookie('lng_ip', $record->longitude, time() + 3600 * 24, '/');
+if (isset($record)) {
+    $country_code = strtolower($record->country_code);
+    setcookie('country_code', $country_code, time() + 3600 * 24, '/');
+    setcookie('lat_ip', $record->latitude, time() + 3600 * 24, '/');
+    setcookie('lng_ip', $record->longitude, time() + 3600 * 24, '/');
+} else {
+    setcookie('country_code', 'at', time() + 3600 * 24, '/');
+    setcookie('lat_ip', '47.809490', time() + 3600 * 24, '/');
+    setcookie('lng_ip', '13.055010', time() + 3600 * 24, '/');
+}
